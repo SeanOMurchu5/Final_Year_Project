42f60ce18095dcce4e62dccb64822f3c84afac30
SeanOMurchu5 <seanomurchu.5201@gmail.com>
2023-03-12 21:18:30 +0000

    Update on design of home activity. Adapters and domains created.

--- app/src/main/java/com/example/finalyearproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/Activity/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.finalyearproject.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.finalyearproject.Adapter.CategoryAdapter;
+import com.example.finalyearproject.Domain.CategoryDomain;
 import com.example.finalyearproject.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category = new ArrayList<>();
+        category.add(new CategoryDomain("Shoes","cat_1"));
+        category.add(new CategoryDomain("Apparel","cat_2"));
+        category.add(new CategoryDomain("Electronics","cat_3"));
+        category.add(new CategoryDomain("Collectibles","cat_4"));
+        category.add(new CategoryDomain("Accessories","cat_5"));
+
+        adapter = new CategoryAdapter(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+
     }
 }
